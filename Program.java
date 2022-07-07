e9ab92af162f68867bcf2cf4f3250fa9bc48c355
kyoumin <86357444+kyoumin@users.noreply.github.com>
2022-07-07 16:17:22 +0900

    part3.ex2.메소드
    static method 와 instance method

---
+++ Program.java
@@ -0,0 +1,65 @@
+package part3.ex2.메소드;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExamList list = new ExamList();
+		
+		list.init();
+		
+		
+		Exam[] exams = new Exam[3];
+//		exams[0] = new Exam();
+		int current = 0;
+		
+		boolean keepLoop = true;
+		int menu;
+		
+		while(keepLoop)
+		{
+			menu = inputMenu();
+			
+			switch(menu)
+			{
+			case 1:
+				
+				//ExamList.inputList(list);
+				list.inputList(); 
+				break;
+				
+			case 2:
+				
+				//ExamList.printList(list);
+				list.printList();
+				break;
+				
+			case 3:
+				System.out.println("Bye~~");
+				
+				keepLoop = false;
+				break;
+				
+			default:
+				System.out.println("잘못된 값을 입력하셨습니다. 메뉴는 1~3까지입니다.");
+			}
+		}
+}
+
+	
+
+	 static int inputMenu() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("┌──────────────────┐");
+		System.out.println("│     메인 메뉴            │");
+		System.out.println("└──────────────────┘");
+		System.out.println("\t1. 성적입력 ");
+		System.out.println("\t2. 성적출력 ");
+		System.out.println("\t3. 종료 ");
+		System.out.println("\t선택> ");
+		int menu = scan.nextInt();
+		return menu;
+	}
+}
